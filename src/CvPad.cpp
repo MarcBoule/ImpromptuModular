a63876d451c90ce99b55713079ffc4310653dbf1
MarcBoule <marc.boule@etsmtl.ca>
2019-11-17 08:56:41 -0500

    continue adding components to Cv-Pad

--- src/CvPad.cpp
+++ src/CvPad.cpp
@@ -14,9 +14,15 @@
 struct CvPad : Module {
 	enum ParamIds {
 		ENUMS(PAD_PARAMS, 16),// touch pads
+		BANK_PARAM,
+		WRITE_PARAM,
+		CV_PARAM,
+		SHARP_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds {
+		BANK_INPUT,
+		CV_INPUT,
 		NUM_INPUTS
 	};
 	enum OutputIds {
@@ -38,14 +44,22 @@ struct CvPad : Module {
 	
 	// Need to save, with reset
 	float cvs[N_BANKS][N_PADS];
+	int readHead;
 
 	// No need to save, with reset
-	int lastPress;
+	// none
 	
 	// No need to save, no reset
 	RefreshCounter refresh;
+	int bank = 0;
 	Trigger padTriggers[N_PADS];
+	Trigger writeTrigger;
 	
+	
+	inline int calcBank() {
+		float bankInputValue = inputs[BANK_INPUT].getVoltage() / 10.0f * (8.0f - 1.0f);
+		return (int) clamp(std::round(params[BANK_PARAM].getValue() + bankInputValue), 0.0f, (8.0f - 1.0f));		
+	}
 	
 	
 	CvPad() {
@@ -54,6 +68,10 @@ struct CvPad : Module {
 		for (int p = 0; p < N_PADS; p++) {
 			configParam(PAD_PARAMS + p, 0.0f, 1.0f, 0.0f, string::f("CV pad %i", p + 1));
 		}
+		configParam(BANK_PARAM, 0.0f, 8.0f - 1.0f, 0.0f, "Bank", "", 0.0f, 1.0f, 1.0f);	// base, multiplier, offset
+		configParam(WRITE_PARAM, 0.0f, 1.0f, 0.0f, "Write");				
+		configParam(CV_PARAM, -INFINITY, INFINITY, 0.0f, "CV");		
+		configParam(SHARP_PARAM, 0.0f, 2.0f, 1.0f, "Volts / Notation");
 		
 		onReset();
 		
@@ -67,10 +85,10 @@ struct CvPad : Module {
 				cvs[b][p] = 0.0f;
 			}
 		}
+		readHead = 0;
 		resetNonJson();
 	}
 	void resetNonJson() {
-		lastPress = 0;
 	}
 	
 	
@@ -93,6 +111,9 @@ struct CvPad : Module {
 		}
 		json_object_set_new(rootJ, "cvs", cvsJ);
 		
+		// readHead
+		json_object_set_new(rootJ, "readHead", json_integer(readHead));
+
 		return rootJ;
 	}
 
@@ -114,29 +135,46 @@ struct CvPad : Module {
 				}
 		}
 
+		// readHead
+		json_t *readHeadJ = json_object_get(rootJ, "readHead");
+		if (readHeadJ)
+			readHead = json_integer_value(readHeadJ);
+		
 		resetNonJson();
 	}
 
 	
 	void process(const ProcessArgs &args) override {		
+		
+		bank = calcBank();
+		
 		if (refresh.processInputs()) {
+			// pads
 			for (int p = 0; p < N_PADS; p++) {
 				if (padTriggers[p].process(params[PAD_PARAMS + p].getValue())) {
-					lastPress = p;
+					readHead = p;
 				}
 				
+			}
+			
+			// write
+			if (writeTrigger.process(params[WRITE_PARAM].getValue())) {
+				cvs[bank][readHead] = inputs[CV_INPUT].getVoltage();
 			}
 		}// userInputs refresh
 		
 		
 		
 		// gate output
-		outputs[GATE_OUTPUTS + 3].setVoltage(padTriggers[lastPress].isHigh() ? 10.0f : 0.0f);
-		outputs[CV_OUTPUTS + 3].setVoltage(cvs[0][lastPress]);
+		outputs[GATE_OUTPUTS + 3].setVoltage(padTriggers[readHead].isHigh() ? 10.0f : 0.0f);
+		outputs[CV_OUTPUTS + 3].setVoltage(cvs[bank][readHead]);
 		
 		// lights
 		if (refresh.processLights()) {
-
+			for (int l = 0; l < 16; l++) {
+				lights[PAD_LIGHTS + l * 2 + 0].setBrightness(readHead == l ? 1.0f : 0.0f);// green
+				lights[PAD_LIGHTS + l * 2 + 1].setBrightness(0.0f);// red
+			}
 		}
 	}
 	
@@ -153,6 +191,77 @@ struct CvPadWidget : ModuleWidget {
 			box.size = Vec(42.0f, 42.0f);
 		}
 	};
+
+	struct BankDisplayWidget : TransparentWidget {
+		CvPad *module;
+		std::shared_ptr<Font> font;
+		
+		BankDisplayWidget() {
+			font = APP->window->loadFont(asset::plugin(pluginInstance, "res/fonts/Segment14.ttf"));
+		}
+
+		void draw(const DrawArgs &args) override {
+			NVGcolor textColor = prepareDisplay(args.vg, &box, 18);
+			nvgFontFaceId(args.vg, font->handle);
+			//nvgTextLetterSpacing(args.vg, 2.5);
+
+			Vec textPos = Vec(6, 24);
+			nvgFillColor(args.vg, nvgTransRGBA(textColor, displayAlpha));
+			nvgText(args.vg, textPos.x, textPos.y, "~", NULL);
+			nvgFillColor(args.vg, textColor);
+			char displayStr[2];
+			unsigned int bank = (unsigned)(module ? module->bank : 0);
+			snprintf(displayStr, 2, "%1u", (unsigned) (bank + 1) );
+			nvgText(args.vg, textPos.x, textPos.y, displayStr, NULL);
+		}
+	};
+
+
+	struct CvDisplayWidget : TransparentWidget {
+		CvPad *module;
+		std::shared_ptr<Font> font;
+		char text[7];
+
+		CvDisplayWidget() {
+			font = APP->window->loadFont(asset::plugin(pluginInstance, "res/fonts/Segment14.ttf"));
+		}
+		
+		void cvToStr(void) {
+			if (module == NULL) {
+				snprintf(text, 7, " 0,000");
+			} 
+			else {
+				int bank = module->calcBank();
+				float cvVal = module->cvs[bank][module->readHead];
+				if (module->params[CvPad::SHARP_PARAM].getValue() > 0.5f) {// show notes
+					text[0] = ' ';
+					printNote(cvVal, &text[1], module->params[CvPad::SHARP_PARAM].getValue() < 1.5f);
+				}
+				else  {// show volts
+					float cvValPrint = std::fabs(cvVal);
+					cvValPrint = (cvValPrint > 9.999f) ? 9.999f : cvValPrint;
+					snprintf(text, 7, " %4.3f", cvValPrint);// Four-wide, three positions after the decimal, left-justified
+					text[0] = (cvVal<0.0f) ? '-' : ' ';
+					text[2] = ',';
+				}
+			}
+		}
+
+		void draw(const DrawArgs &args) override {
+			NVGcolor textColor = prepareDisplay(args.vg, &box, 18);
+			nvgFontFaceId(args.vg, font->handle);
+			nvgTextLetterSpacing(args.vg, -1.5);
+
+			Vec textPos = Vec(6, 24);
+			nvgFillColor(args.vg, nvgTransRGBA(textColor, displayAlpha));
+			nvgText(args.vg, textPos.x, textPos.y, "~~~~~~", NULL);
+			nvgFillColor(args.vg, textColor);
+			cvToStr();
+			nvgText(args.vg, textPos.x, textPos.y, text, NULL);
+		}
+	};
+
+
 	
 	CvPadWidget(CvPad *module) {
 		setModule(module);
@@ -172,10 +281,13 @@ struct CvPadWidget : ModuleWidget {
 		addChild(createDynamicWidget<IMScrew>(Vec(15, 365), module ? &module->panelTheme : NULL));
 		addChild(createDynamicWidget<IMScrew>(Vec(box.size.x-30, 365), module ? &module->panelTheme : NULL));
 				
+				
 		// pads and lights
-		static const int padX = 146 + 21;
+		// ----------------------------
+		
+		static const int padX = 71 + 21;
 		static const int padXd = 68;
-		static const int padY = 84 + 21;
+		static const int padY = 94 + 21;
 		static const int padYd = padXd;
 		static const int ledOffsetY = 30;
 		for (int y = 0; y < 4; y++) {
@@ -183,19 +295,63 @@ struct CvPadWidget : ModuleWidget {
 				// pad
 				addParam(createParamCentered<Pad>(Vec(padX + padXd * x, padY + padYd * y), module, CvPad::PAD_PARAMS + y * 4 + x));
 				// light
-				addChild(createLightCentered<MediumLight<GreenRedLight>>(Vec(padX + padXd * x, padY + padYd * y - ledOffsetY), module, CvPad::PAD_LIGHTS + y * 4 + x));
+				addChild(createLightCentered<MediumLight<GreenRedLight>>(Vec(padX + padXd * x, padY + padYd * y - ledOffsetY), module, CvPad::PAD_LIGHTS + (y * 4 + x) * 2 + 0));
 			}
 		}
 		
+		
+		// left side column
+		// ----------------------------
+		
+		static const int leftX = 35;
+		static const int topY = padY - 55;
+		// bank display
+		BankDisplayWidget *displayBank = new BankDisplayWidget();
+		displayBank->box.size = Vec(24, 30);// 1 character
+		displayBank->box.pos = Vec(leftX, topY);
+		displayBank->box.pos = displayBank->box.pos.minus(displayBank->box.size.div(2));// centering
+		displayBank->module = module;
+		addChild(displayBank);	
+		// bank knob
+		addParam(createDynamicParamCentered<IMSmallSnapKnob>(Vec(leftX, padY), module, CvPad::BANK_PARAM, module ? &module->panelTheme : NULL));
+		// write button
+		addParam(createDynamicParamCentered<IMBigPushButton>(Vec(leftX, padY + padYd * 1), module, CvPad::WRITE_PARAM, module ? &module->panelTheme : NULL));	
+		// cv input
+		addInput(createDynamicPortCentered<IMPort>(Vec(leftX, padY + padYd * 2), true, module, CvPad::CV_INPUT, module ? &module->panelTheme : NULL));
+		// bank cv input
+		addInput(createDynamicPortCentered<IMPort>(Vec(leftX, padY + padYd * 3), true, module, CvPad::BANK_INPUT, module ? &module->panelTheme : NULL));
+		
+		
+		// right side column
+		// ----------------------------
+		
+		static const int rightX = 379;
+		// cv display
+		CvDisplayWidget *displayCv = new CvDisplayWidget();
+		displayCv->box.size = Vec(98, 30);// 6 characters (ex.: "-1,234")
+		displayCv->box.pos = Vec(rightX, topY);
+		displayCv->box.pos = displayCv->box.pos.minus(displayCv->box.size.div(2));// centering
+		displayCv->module = module;
+		addChild(displayCv);
+		// cv knob
+		addParam(createDynamicParamCentered<IMBigKnobInf>(Vec(rightX, padY), module, CvPad::CV_PARAM, module ? &module->panelTheme : NULL));
 		// outputs
-		static const int outX = 435;
-		static const int outY = padY;
-		static const int outYd = padYd;
+		static const int outY = padY + padYd * 3;
+		static const int outYd = 45;
 		for (int y = 0; y < 4; y++) {
-			addOutput(createDynamicPortCentered<IMPort>(Vec(outX, outY + outYd * y), false, module, CvPad::CV_OUTPUTS + y, module ? &module->panelTheme : NULL));
-			addOutput(createDynamicPortCentered<IMPort>(Vec(outX + 37, outY + outYd * y), false, module, CvPad::GATE_OUTPUTS + y, module ? &module->panelTheme : NULL));
+			addOutput(createDynamicPortCentered<IMPort>(Vec(rightX - 19, outY - outYd * (3 - y)), false, module, CvPad::CV_OUTPUTS + y, module ? &module->panelTheme : NULL));
+			addOutput(createDynamicPortCentered<IMPort>(Vec(rightX + 19, outY - outYd * (3 - y)), false, module, CvPad::GATE_OUTPUTS + y, module ? &module->panelTheme : NULL));
 		}
+		
+		
+		// top of pad, first row
+		// ----------------------------
+
+		// Volt/sharp/flat switch
+		addParam(createParamCentered<CKSSThreeInvNoRandom>(Vec(padX + padXd * 1, topY), module, CvPad::SHARP_PARAM));
+		
 	}
+	
 	
 	void step() override {
 		if (module) {
