b4e256e7498f9cac8cbc1f2522d3593f1d225051
MarcBoule <marc.boule@etsmtl.ca>
2020-01-31 16:03:00 -0500

    HK add better keyprinting and prepare new key print method

--- src/Hotkey.cpp
+++ src/Hotkey.cpp
@@ -13,6 +13,167 @@
 #include "ImpromptuModular.hpp"
 
 
+// the next two functions are copied from Rack/dep/glfw/tests/events.c
+static const char* get_key_name(int key)
+{
+    switch (key)
+    {
+        // Printable keys
+        case GLFW_KEY_A:            return "A";
+        case GLFW_KEY_B:            return "B";
+        case GLFW_KEY_C:            return "C";
+        case GLFW_KEY_D:            return "D";
+        case GLFW_KEY_E:            return "E";
+        case GLFW_KEY_F:            return "F";
+        case GLFW_KEY_G:            return "G";
+        case GLFW_KEY_H:            return "H";
+        case GLFW_KEY_I:            return "I";
+        case GLFW_KEY_J:            return "J";
+        case GLFW_KEY_K:            return "K";
+        case GLFW_KEY_L:            return "L";
+        case GLFW_KEY_M:            return "M";
+        case GLFW_KEY_N:            return "N";
+        case GLFW_KEY_O:            return "O";
+        case GLFW_KEY_P:            return "P";
+        case GLFW_KEY_Q:            return "Q";
+        case GLFW_KEY_R:            return "R";
+        case GLFW_KEY_S:            return "S";
+        case GLFW_KEY_T:            return "T";
+        case GLFW_KEY_U:            return "U";
+        case GLFW_KEY_V:            return "V";
+        case GLFW_KEY_W:            return "W";
+        case GLFW_KEY_X:            return "X";
+        case GLFW_KEY_Y:            return "Y";
+        case GLFW_KEY_Z:            return "Z";
+        case GLFW_KEY_1:            return "1";
+        case GLFW_KEY_2:            return "2";
+        case GLFW_KEY_3:            return "3";
+        case GLFW_KEY_4:            return "4";
+        case GLFW_KEY_5:            return "5";
+        case GLFW_KEY_6:            return "6";
+        case GLFW_KEY_7:            return "7";
+        case GLFW_KEY_8:            return "8";
+        case GLFW_KEY_9:            return "9";
+        case GLFW_KEY_0:            return "0";
+        case GLFW_KEY_SPACE:        return "SPACE";
+        case GLFW_KEY_MINUS:        return "MINUS";
+        case GLFW_KEY_EQUAL:        return "EQUAL";
+        case GLFW_KEY_LEFT_BRACKET: return "LEFT BRACKET";
+        case GLFW_KEY_RIGHT_BRACKET: return "RIGHT BRACKET";
+        case GLFW_KEY_BACKSLASH:    return "BACKSLASH";
+        case GLFW_KEY_SEMICOLON:    return "SEMICOLON";
+        case GLFW_KEY_APOSTROPHE:   return "APOSTROPHE";
+        case GLFW_KEY_GRAVE_ACCENT: return "GRAVE ACCENT";
+        case GLFW_KEY_COMMA:        return "COMMA";
+        case GLFW_KEY_PERIOD:       return "PERIOD";
+        case GLFW_KEY_SLASH:        return "SLASH";
+        case GLFW_KEY_WORLD_1:      return "WORLD 1";
+        case GLFW_KEY_WORLD_2:      return "WORLD 2";
+
+        // Function keys
+        case GLFW_KEY_ESCAPE:       return "ESCAPE";
+        case GLFW_KEY_F1:           return "F1";
+        case GLFW_KEY_F2:           return "F2";
+        case GLFW_KEY_F3:           return "F3";
+        case GLFW_KEY_F4:           return "F4";
+        case GLFW_KEY_F5:           return "F5";
+        case GLFW_KEY_F6:           return "F6";
+        case GLFW_KEY_F7:           return "F7";
+        case GLFW_KEY_F8:           return "F8";
+        case GLFW_KEY_F9:           return "F9";
+        case GLFW_KEY_F10:          return "F10";
+        case GLFW_KEY_F11:          return "F11";
+        case GLFW_KEY_F12:          return "F12";
+        case GLFW_KEY_F13:          return "F13";
+        case GLFW_KEY_F14:          return "F14";
+        case GLFW_KEY_F15:          return "F15";
+        case GLFW_KEY_F16:          return "F16";
+        case GLFW_KEY_F17:          return "F17";
+        case GLFW_KEY_F18:          return "F18";
+        case GLFW_KEY_F19:          return "F19";
+        case GLFW_KEY_F20:          return "F20";
+        case GLFW_KEY_F21:          return "F21";
+        case GLFW_KEY_F22:          return "F22";
+        case GLFW_KEY_F23:          return "F23";
+        case GLFW_KEY_F24:          return "F24";
+        case GLFW_KEY_F25:          return "F25";
+        case GLFW_KEY_UP:           return "UP";
+        case GLFW_KEY_DOWN:         return "DOWN";
+        case GLFW_KEY_LEFT:         return "LEFT";
+        case GLFW_KEY_RIGHT:        return "RIGHT";
+        case GLFW_KEY_LEFT_SHIFT:   return "LEFT SHIFT";
+        case GLFW_KEY_RIGHT_SHIFT:  return "RIGHT SHIFT";
+        case GLFW_KEY_LEFT_CONTROL: return "LEFT CONTROL";
+        case GLFW_KEY_RIGHT_CONTROL: return "RIGHT CONTROL";
+        case GLFW_KEY_LEFT_ALT:     return "LEFT ALT";
+        case GLFW_KEY_RIGHT_ALT:    return "RIGHT ALT";
+        case GLFW_KEY_TAB:          return "TAB";
+        case GLFW_KEY_ENTER:        return "ENTER";
+        case GLFW_KEY_BACKSPACE:    return "BACKSPACE";
+        case GLFW_KEY_INSERT:       return "INSERT";
+        case GLFW_KEY_DELETE:       return "DELETE";
+        case GLFW_KEY_PAGE_UP:      return "PAGE UP";
+        case GLFW_KEY_PAGE_DOWN:    return "PAGE DOWN";
+        case GLFW_KEY_HOME:         return "HOME";
+        case GLFW_KEY_END:          return "END";
+        case GLFW_KEY_KP_0:         return "KEYPAD 0";
+        case GLFW_KEY_KP_1:         return "KEYPAD 1";
+        case GLFW_KEY_KP_2:         return "KEYPAD 2";
+        case GLFW_KEY_KP_3:         return "KEYPAD 3";
+        case GLFW_KEY_KP_4:         return "KEYPAD 4";
+        case GLFW_KEY_KP_5:         return "KEYPAD 5";
+        case GLFW_KEY_KP_6:         return "KEYPAD 6";
+        case GLFW_KEY_KP_7:         return "KEYPAD 7";
+        case GLFW_KEY_KP_8:         return "KEYPAD 8";
+        case GLFW_KEY_KP_9:         return "KEYPAD 9";
+        case GLFW_KEY_KP_DIVIDE:    return "KEYPAD DIVIDE";
+        case GLFW_KEY_KP_MULTIPLY:  return "KEYPAD MULTIPLY";
+        case GLFW_KEY_KP_SUBTRACT:  return "KEYPAD SUBTRACT";
+        case GLFW_KEY_KP_ADD:       return "KEYPAD ADD";
+        case GLFW_KEY_KP_DECIMAL:   return "KEYPAD DECIMAL";
+        case GLFW_KEY_KP_EQUAL:     return "KEYPAD EQUAL";
+        case GLFW_KEY_KP_ENTER:     return "KEYPAD ENTER";
+        case GLFW_KEY_PRINT_SCREEN: return "PRINT SCREEN";
+        case GLFW_KEY_NUM_LOCK:     return "NUM LOCK";
+        case GLFW_KEY_CAPS_LOCK:    return "CAPS LOCK";
+        case GLFW_KEY_SCROLL_LOCK:  return "SCROLL LOCK";
+        case GLFW_KEY_PAUSE:        return "PAUSE";
+        case GLFW_KEY_LEFT_SUPER:   return "LEFT SUPER";
+        case GLFW_KEY_RIGHT_SUPER:  return "RIGHT SUPER";
+        case GLFW_KEY_MENU:         return "MENU";
+
+        default:                    return "UNKNOWN";
+    }
+}
+static const char* get_mods_name(int mods)
+{
+    static char name[512];
+
+    if (mods == 0)
+        return " no mods";
+
+    name[0] = '\0';
+
+    if (mods & GLFW_MOD_SHIFT)
+        strcat(name, " shift");
+    if (mods & GLFW_MOD_CONTROL)
+        strcat(name, " control");
+    if (mods & GLFW_MOD_ALT)
+        strcat(name, " alt");
+    if (mods & GLFW_MOD_SUPER)
+        strcat(name, " super");
+    if (mods & GLFW_MOD_CAPS_LOCK)
+        strcat(name, " capslock-on");
+    if (mods & GLFW_MOD_NUM_LOCK)
+        strcat(name, " numlock-on");
+
+    return name;
+}
+
+
+//*****************************************************************************
+
+
 struct Hotkey : Module {
 	enum ParamIds {
 		RECORD_KEY_PARAM,
@@ -32,15 +193,18 @@ struct Hotkey : Module {
 	};
 	
 	
+	// Constants
+	static constexpr float maxDelay = 1.0f;// in seconds
+		
 	// Need to save, no reset
 	int panelTheme;
 	
 	// Need to save, with reset
-	char hotkey;
-	bool requestTrig;
+	int hotkey;
+	bool requestTrig;// will stay high as long as delayCnt is not at 0
 
 	// No need to save, with reset
-	// none
+	unsigned long delayCnt;// emit pulse when reaches 0, 
 	
 	// No need to save, no reset
 	dsp::PulseGenerator trigOutPulse;
@@ -51,6 +215,7 @@ struct Hotkey : Module {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 
 		configParam(RECORD_KEY_PARAM, 0.0f, 1.0f, 0.0f, "Record hotkey");
+		configParam(DELAY_PARAM, 0.0f, maxDelay, 0.0f, "Delay");
 		onReset();
 		
 		panelTheme = (loadDarkAsDefault() ? 1 : 0);
@@ -63,6 +228,7 @@ struct Hotkey : Module {
 		resetNonJson(false);
 	}
 	void resetNonJson(bool delayed) {// delay thread sensitive parts (i.e. schedule them so that process() will do them)
+		delayCnt = 0;
 	}
 	
 	void onRandomize() override {
@@ -103,16 +269,17 @@ struct Hotkey : Module {
 	}		
 	
 	
-	bool hotkeyPressed(int newKey) {// return true if processed the key	
+	bool hotkeyPressed(int newCodepoint) {// return true if processed the key	
 		bool processed = false;
 		
 		if (params[RECORD_KEY_PARAM].getValue() >= 0.5f) {// if recording the keypress
-			hotkey = (char)newKey;
+			hotkey = newCodepoint;
 			params[RECORD_KEY_PARAM].setValue(0.0f);
 			processed = true;
 		}
 		else {// normal key press when not recording
-			if (newKey == hotkey) {
+			if (newCodepoint == hotkey) {
+				delayCnt = (unsigned long)(APP->engine->getSampleRate() * maxDelay * params[DELAY_PARAM].getValue());
 				requestTrig = true;
 				processed = true;
 			}
@@ -120,6 +287,26 @@ struct Hotkey : Module {
 		
 		return processed;
 	}
+
+/*	bool hotkeyPressed(int newKey, int newMods) {// return true if processed the key	
+		bool processed = false;
+		INFO("key = %s, mod = %s", get_key_name(newKey), get_mods_name(newMods));
+		
+		if (params[RECORD_KEY_PARAM].getValue() >= 0.5f) {// if recording the keypress
+			hotkey = newKey;
+			params[RECORD_KEY_PARAM].setValue(0.0f);
+			processed = true;
+		}
+		else {// normal key press when not recording
+			if (newKey == hotkey) {
+				delayCnt = (unsigned long)(APP->engine->getSampleRate() * maxDelay * params[DELAY_PARAM].getValue());
+				requestTrig = true;
+				processed = true;
+			}
+		}
+		
+		return processed;
+	}*/
 
 
 	void process(const ProcessArgs &args) override {
@@ -130,10 +317,11 @@ struct Hotkey : Module {
 		}// userInputs refresh
 
 
-		if (requestTrig) {
+		if (requestTrig && (delayCnt == 0)) {
 			trigOutPulse.trigger(0.002f);
 			requestTrig = false;
 		}
+		
 
 		outputs[TRIG_OUTPUT].setVoltage((trigOutPulse.process(args.sampleTime) ? 10.0f : 0.0f));
 		
@@ -141,6 +329,10 @@ struct Hotkey : Module {
 		if (refresh.processLights()) {
 			lights[RECORD_KEY_LIGHT].setBrightness(params[RECORD_KEY_PARAM].getValue());
 		}// lightRefreshCounter
+		
+		if (delayCnt > 0) {
+			delayCnt--;
+		}
 	}// process()
 	
 };
@@ -180,8 +372,13 @@ struct HotkeyWidget : ModuleWidget {
 		menu->addChild(settingsLabel);
 		
 		MenuLabel *hotkeyLabel = new MenuLabel();
-		char strBuf[32];
-		snprintf(strBuf, 32, "Current hotkey: '%c'", module->hotkey);
+		char strBuf[32]; 
+		if (module->hotkey == ' ') {
+			snprintf(strBuf, 32, "Current hotkey: [spacebar]");
+		}
+		else {
+			snprintf(strBuf, 32, "Current hotkey: '%lc'", module->hotkey);
+		}
 		hotkeyLabel->text = strBuf;
 		menu->addChild(hotkeyLabel);
 	}	
@@ -201,21 +398,25 @@ struct HotkeyWidget : ModuleWidget {
 		
 		// Screws
 		addChild(createDynamicWidget<IMScrew>(Vec(15, 0), NULL));//module ? &module->panelTheme : NULL));
-		// addChild(createDynamicWidget<IMScrew>(Vec(15, 365), module ? &module->panelTheme : NULL));
+		addChild(createDynamicWidget<IMScrew>(Vec(15, 365), module ? &module->panelTheme : NULL));
 		// addChild(createDynamicWidget<IMScrew>(Vec(box.size.x-30, 0), module ? &module->panelTheme : NULL));
-		addChild(createDynamicWidget<IMScrew>(Vec(box.size.x-30, 365), NULL));//module ? &module->panelTheme : NULL));
+		// addChild(createDynamicWidget<IMScrew>(Vec(box.size.x-30, 365), NULL));//module ? &module->panelTheme : NULL));
 
 
-
+		static const float centerX = 22.5f;
 		
 		// Record-key LED bezel and light
+		static const int buttonY = 80;
 		SvgSwitch *ledBez;
-		addParam(ledBez = createParamCentered<LEDBezel>(Vec(30.0f, 50.0f), module, Hotkey::RECORD_KEY_PARAM));
+		addParam(ledBez = createParamCentered<LEDBezel>(Vec(centerX, buttonY), module, Hotkey::RECORD_KEY_PARAM));
 		ledBez->momentary = false;
-		addChild(createLightCentered<MuteLight<RedLight>>(Vec(30.0f, 50.0f), module, Hotkey::RECORD_KEY_LIGHT));
+		addChild(createLightCentered<MuteLight<RedLight>>(Vec(centerX, buttonY), module, Hotkey::RECORD_KEY_LIGHT));
+		
+		// Delay knob
+		addParam(createDynamicParamCentered<IMSmallKnob>(Vec(centerX, 220.0f), module, Hotkey::DELAY_PARAM, NULL));//module ? &module->panelTheme : NULL));
 
 		// trig out
-		addOutput(createDynamicPortCentered<IMPort>(Vec(30.0f, 380.0f - mm2px(31.25f)), false, module, Hotkey::TRIG_OUTPUT, NULL));//module ? &module->panelTheme : NULL));
+		addOutput(createDynamicPortCentered<IMPort>(Vec(centerX, 380.0f - mm2px(31.25f)), false, module, Hotkey::TRIG_OUTPUT, NULL));//module ? &module->panelTheme : NULL));
 	}
 	
 	// void step() override {
@@ -226,16 +427,26 @@ struct HotkeyWidget : ModuleWidget {
 		// Widget::step();
 	// }
 	
-	void onHoverKey(const event::HoverKey& e) override {
+	void onHoverText(const event::HoverText& e) override {
+		Hotkey *module = dynamic_cast<Hotkey*>(this->module);
+		if (module->hotkeyPressed(e.codepoint)) {
+			e.consume(this);
+			return;
+		}
+		ModuleWidget::onHoverText(e); 
+	}
+	
+/*	void onHoverKey(const event::HoverKey& e) override {
 		if (e.action == GLFW_PRESS) {
 			Hotkey *module = dynamic_cast<Hotkey*>(this->module);
-			if ((e.mods & RACK_MOD_MASK) == 0 && module->hotkeyPressed(e.key)) {
+			if (module->hotkeyPressed(e.key, e.mods)) {
 				e.consume(this);
 				return;
 			}
 		}
 		ModuleWidget::onHoverKey(e); 
-	}
+	}*/
+	
 };
 
 Model *modelHotkey = createModel<Hotkey, HotkeyWidget>("Hotkey");
