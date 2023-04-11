59969fa186181807556534768de6f5666559b23a
MarcBoule <marc.boule@etsmtl.ca>
2023-04-11 15:25:49 -0400

    fix for issue#67

--- src/ImpromptuModular.hpp
+++ src/ImpromptuModular.hpp
@@ -137,7 +137,17 @@ struct RefreshCounter {
 };
 
 
-struct Trigger : dsp::SchmittTrigger {
+struct Trigger {// : dsp::SchmittTrigger {
+	bool state = true;
+
+	void reset() {
+		state = true;
+	}
+	
+	bool isHigh() {
+		return state;
+	}
+	
 	// implements a 0.1V - 1.0V SchmittTrigger (see include/dsp/digital.hpp) instead of 
 	//   calling SchmittTriggerInstance.process(math::rescale(in, 0.1f, 1.f, 0.f, 1.f))
 	bool process(float in) {
