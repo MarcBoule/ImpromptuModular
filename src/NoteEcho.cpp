7192a3391a134d95f5f7ecb29cc410f2ad08810b
MarcBoule <marc.boule@etsmtl.ca>
2024-01-27 20:32:46 -0500

    NE implement knob displays

--- src/NoteEcho.cpp
+++ src/NoteEcho.cpp
@@ -49,6 +49,7 @@ struct NoteEcho : Module {
 
 	// Constants
 	static const int length = 32;// must be at least 16 given tap init values
+	static constexpr float delayInfoTime = 3.0f;// seconds
 	
 	
 	// Need to save, no reset
@@ -59,13 +60,21 @@ struct NoteEcho : Module {
 	int sampDelayClk;
 
 	// No need to save, with reset
-	long dispModes[3];// 0 = TAP, 1 = ST, 2 = VEL
+	int notifySource[3] = {0, 0, 0};// 0 (not really used), 1 (semi) 2 (vel)
+	long notifyInfo[3] = {0l, 0l, 0l};// downward step counter when semi or vel to be displayed, 0 when normal tap display
 	
 	// No need to save, no reset
 	RefreshCounter refresh;
 	Trigger clkTrigger;
 	Trigger sdTrigger;
 
+
+	int getTapValue(int tapNum) {
+		return (int)(params[TAP_PARAMS + tapNum].getValue() + 0.5f);
+	}
+	int getSemiValue(int tapNum) {
+		return (int)(std::round(params[ST_PARAMS + tapNum].getValue()));
+	}
 	
 	NoteEcho() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
@@ -86,7 +95,7 @@ struct NoteEcho : Module {
 			paramQuantities[ST_PARAMS + i]->snapEnabled = true;
 			// vel knobs
 			snprintf(strBuf, 32, "Tap %i velocity percent", i + 1);
-			configParam(VEL_PARAMS + i, 0.0f, 100.0f, 0.0f, strBuf);		
+			configParam(VEL_PARAMS + i, 0.0f, 1.0f, 0.0f, strBuf, " %", 0.0f, 100.0f);		
 		}
 		
 		configInput(CV_INPUT, "CV");
@@ -117,10 +126,7 @@ struct NoteEcho : Module {
 		sampDelayClk = 0;
 		resetNonJson(false);
 	}
-	void resetNonJson(bool delayed) {// delay thread sensitive parts (i.e. schedule them so that process() will do them)
-		for (int i = 0; i < 3; i++) {
-			dispModes[i] = 0l;
-		}
+	void resetNonJson(bool delayed) {
 	}
 
 	
@@ -186,12 +192,11 @@ struct NoteEcho : Module {
 			// sampDelayClk light
 			lights[SD_LIGHT].setBrightness(sampDelayClk != 0 ? 1.0f : 0.0f);
 
-			// display modes counters
+			// info notification counters
 			for (int i = 0; i < 3; i++) {
-				dispModes[i]--;
-				if (dispModes[i] < 0l) {
-					dispModes[i] = 0l;
-				}
+				notifyInfo[i]--;
+				if (notifyInfo[i] < 0l)
+					notifyInfo[i] = 0l;
 			}
 		}// lightRefreshCounter
 	}// process()
@@ -200,45 +205,49 @@ struct NoteEcho : Module {
 
 struct NoteEchoWidget : ModuleWidget {
 	struct TapKnob : IMMediumKnob {
-		// int *displayIndexPtr = NULL;
-		// void onButton(const event::Button& e) override {
-			// const ParamQuantity* paramQuantity = getParamQuantity();
-			// if (paramQuantity && displayIndexPtr) {
-				// *displayIndexPtr = 0;
-			// }
-			// IMMediumKnob::onButton(e);
-		// }
+		NoteEcho *module = nullptr;
+		int tapNum = 0;
+		void onDragMove(const event::DragMove &e) override {
+			if (module) {
+				module->notifyInfo[tapNum] = 0l;
+			}
+			IMMediumKnob::onDragMove(e);
+		}
 	};
 	struct SemitoneKnob : IMMediumKnob {
-		// int *displayIndexPtr = NULL;
-		// void onButton(const event::Button& e) override {
-			// const ParamQuantity* paramQuantity = getParamQuantity();
-			// if (paramQuantity && displayIndexPtr) {
-				// *displayIndexPtr = 0;
-			// }
-			// IMMediumKnob::onButton(e);
-		// }
+		NoteEcho *module = nullptr;
+		int tapNum = 0;
+		void onDragMove(const event::DragMove &e) override {
+			if (module) {
+				module->notifySource[tapNum] = 1l;
+				module->notifyInfo[tapNum] = (long) (NoteEcho::delayInfoTime * APP->engine->getSampleRate() / RefreshCounter::displayRefreshStepSkips);
+			}
+			IMMediumKnob::onDragMove(e);
+		}
 	};
 	struct VelKnob : IMMediumKnob {
-		// int *displayIndexPtr = NULL;
-		// void onButton(const event::Button& e) override {
-			// const ParamQuantity* paramQuantity = getParamQuantity();
-			// if (paramQuantity && displayIndexPtr) {
-				// *displayIndexPtr = 0;
-			// }
-			// IMMediumKnob::onButton(e);
-		// }
+		NoteEcho *module = nullptr;
+		int tapNum = 0;
+		void onDragMove(const event::DragMove &e) override {
+			if (module) {
+				module->notifySource[tapNum] = 2l;
+				module->notifyInfo[tapNum] = (long) (NoteEcho::delayInfoTime * APP->engine->getSampleRate() / RefreshCounter::displayRefreshStepSkips);
+			}
+			IMMediumKnob::onDragMove(e);
+		}
 	};
 	
 	struct TapDisplayWidget : TransparentWidget {// a centered display, must derive from this
 		NoteEcho *module = nullptr;
+		int tapNum = 0;
 		std::shared_ptr<Font> font;
 		std::string fontPath;
 		char displayStr[16] = {};
 		static const int textFontSize = 15;
 		static constexpr float textOffsetY = 19.9f; // 18.2f for 14 pt, 19.7f for 15pt
 		
-		TapDisplayWidget(Vec _pos, Vec _size, NoteEcho *_module) {
+		TapDisplayWidget(int _tapNum, Vec _pos, Vec _size, NoteEcho *_module) {
+			tapNum = _tapNum;
 			box.size = _size;
 			box.pos = _pos.minus(_size.div(2));
 			module = _module;
@@ -260,7 +269,28 @@ struct NoteEchoWidget : ModuleWidget {
 				nvgText(args.vg, textPos.x, textPos.y, initString.c_str(), NULL);
 				
 				nvgFillColor(args.vg, displayColOn);
-				snprintf(displayStr, 4, "CPY");
+				if (!module || module->getTapValue(tapNum) < 1) {
+					snprintf(displayStr, 4, " - ");
+				}
+				else if (module->notifyInfo[tapNum] > 0l) {
+					if (module->notifySource[tapNum] == 1) {
+						// semitone
+						int ist = module->getSemiValue(tapNum);
+						snprintf(displayStr, 4, " %2u", (unsigned)(std::abs(ist)));
+						if (ist != 0) {
+							displayStr[0] = ist > 0 ? '+' : '-';
+						}
+					}
+					else {
+						// vel
+						float vel = module->params[NoteEcho::VEL_PARAMS + tapNum].getValue();
+						unsigned int ivel100 = (unsigned)(std::round(vel * 100.0f));
+						snprintf(displayStr, 4, "%3u", ivel100);
+					}
+				}
+				else {
+					snprintf(displayStr, 4, "T%2u", (unsigned)(module->getTapValue(tapNum)));	
+				}
 				nvgText(args.vg, textPos.x, textPos.y, displayStr, NULL);
 			}
 		}
@@ -328,19 +358,30 @@ struct NoteEchoWidget : ModuleWidget {
 		addInput(createDynamicPortCentered<IMPort>(mm2px(Vec(col52, row0)), true, module, NoteEcho::GATE_INPUT, mode));
 		addInput(createDynamicPortCentered<IMPort>(mm2px(Vec(col53, row0)), true, module, NoteEcho::VEL_INPUT, mode));
 		addInput(createDynamicPortCentered<IMPort>(mm2px(Vec(col54, row0)), true, module, NoteEcho::CLK_INPUT, mode));
-		addParam(createDynamicParamCentered<IMPushButton>(mm2px(Vec(col55, row0)), module, NoteEcho::SD_PARAM, mode));
+		addParam(createParamCentered<LEDButton>(mm2px(Vec(col55, row0)), module, NoteEcho::SD_PARAM));
+		addChild(createLightCentered<MediumLight<GreenLightIM>>(mm2px(Vec(col55, row0)), module, NoteEcho::SD_LIGHT));
 		
-		// row 1-3
+		// rows 1-3
 		for (int i = 0; i < 3; i++) {
-			addParam(createDynamicParamCentered<TapKnob>(mm2px(Vec(col41, row1 + i * row23d)), module, NoteEcho::TAP_PARAMS + i, mode));	
+			TapKnob* tapKnobP;
+			addParam(tapKnobP = createDynamicParamCentered<TapKnob>(mm2px(Vec(col41, row1 + i * row23d)), module, NoteEcho::TAP_PARAMS + i, mode));	
+			tapKnobP->module = module;
+			tapKnobP->tapNum = i;
 			
 			// displays
-			TapDisplayWidget* tapDisplayWidget = new TapDisplayWidget(mm2px(Vec(col42, row1 + i * row23d)), VecPx(displayWidths, displayHeights), module);
+			TapDisplayWidget* tapDisplayWidget = new TapDisplayWidget(i, mm2px(Vec(col42, row1 + i * row23d)), VecPx(displayWidths, displayHeights), module);
 			addChild(tapDisplayWidget);
 			svgPanel->fb->addChild(new DisplayBackground(tapDisplayWidget->box.pos, tapDisplayWidget->box.size, mode));
 			
-			addParam(createDynamicParamCentered<SemitoneKnob>(mm2px(Vec(col43, row1 + i * row23d)), module, NoteEcho::ST_PARAMS + i, mode));	
-			addParam(createDynamicParamCentered<VelKnob>(mm2px(Vec(col44, row1 + i * row23d)), module, NoteEcho::VEL_PARAMS + i, mode));	
+			SemitoneKnob* stKnobP;
+			addParam(stKnobP = createDynamicParamCentered<SemitoneKnob>(mm2px(Vec(col43, row1 + i * row23d)), module, NoteEcho::ST_PARAMS + i, mode));	
+			stKnobP->module = module;
+			stKnobP->tapNum = i;
+			
+			VelKnob* velKnobP;			
+			addParam(velKnobP = createDynamicParamCentered<VelKnob>(mm2px(Vec(col44, row1 + i * row23d)), module, NoteEcho::VEL_PARAMS + i, mode));	
+			velKnobP->module = module;
+			velKnobP->tapNum = i;
 		}
 		
 		// row 4
@@ -354,8 +395,19 @@ struct NoteEchoWidget : ModuleWidget {
 		addOutput(createDynamicPortCentered<IMPort>(mm2px(Vec(col52, row5)), false, module, NoteEcho::GATE_OUTPUT, mode));
 		addOutput(createDynamicPortCentered<IMPort>(mm2px(Vec(col53, row5)), false, module, NoteEcho::VEL_OUTPUT, mode));
 		addOutput(createDynamicPortCentered<IMPort>(mm2px(Vec(col54, row5)), false, module, NoteEcho::CLK_OUTPUT, mode));
+
 		// gate light matrix
-		// TODO
+		static const float gldx = 2.0f;
+		static const float gldy = 2.0f;
+		float posx = col55 - gldx * 1.5f - 1.0f;
+		for (int i = 0; i < 4; i++) {
+			float posy = row5 - gldy * 1.5f;
+			for (int j = 0; j < 4; j++) {
+				addChild(createLightCentered<TinyLight<GreenLight>>(mm2px(Vec(posx, posy)), module, NoteEcho::GATE_LIGHTS + i));
+				posy += gldy;
+			}
+			posx += gldx;
+		}
 	}
 };
 
